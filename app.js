774018fbcfd102b8172fb71577a6986ae802f710
gonzalotaritolay94@gmail.com <gonzalotaritolay94@gmail.com>
2023-08-08 04:33:09 -0300

    subiendo mercado liebre

---
+++ app.js
@@ -0,0 +1,17 @@
+const express= require("express")
+const app= express()
+const path=require("path")
+//app.use(express.static("../public"));
+app.use('/static', express.static(__dirname + '/public'));
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"views/home.html"))
+ })
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "views/register.html"));
+});
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "views/login.html"));
+});
+app.listen(3030, ()=>{ 
+    console.log("levantado")
+})
